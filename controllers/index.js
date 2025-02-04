ba4269dec1caea57ab23520ae49b42b872bef8b4
Anthony <=>
2025-02-04 14:11:12 +0100

    👌 IMPROVE: SUPP charps to index?js controller

--- controllers/index.js
+++ controllers/index.js
@@ -21,11 +21,3 @@ export const searchAlbum = async (req, res) => {
         res.status(500).json({ error: "Erreur interne du serveur." });
     }
 };
-
-export const getCharts = async (req, res) => {
-    const { name } = req.params;
-    fetch('https://api.deezer.com/chart')
-        .then(response => response.json()) // Convertir la réponse en JSON
-        .then(data => console.log(data)) // Afficher les données
-        .catch(error => console.error('Erreur :', error)); // Gérer les erreurs
-};
