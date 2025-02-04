b7ee2caa267fc2c6d971eaf650b4c7577f0bc298
Anthony <anthony20021@gmail>
2025-02-04 10:56:33 +0100

    📦 NEW: première fonction controller

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { firstController } from '../controllers/index.js';
 
 const router = express.Router();
 
-router.get('', (req, res) => {
-    res.send("Bienvenue dans l'API musique !");
-});
+router.get('', firstController);
 
 export default router;
