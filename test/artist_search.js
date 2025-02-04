a8bfe680f58b8fbfb88fcfd4ab1cc504a394bf60
shyrin <shyrinbelaloui@gmail.com>
2025-02-04 11:17:00 +0100

    📦 NEW: add file test

---
+++ test/artist_search.js
@@ -0,0 +1,19 @@
+import request from "supertest";
+import app from "../server.js";
+
+describe("Recherche d'artiste avec Deezer API", () => {
+    it("devrait retourner des résultats pour un artiste donné", async () => {
+        const response = await request(app).get("/search?artist=eminem");
+        
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data.length).toBeGreaterThan(0);
+    });
+
+    it("devrait retourner une erreur si l'artiste n'est pas fourni", async () => {
+        const response = await request(app).get("/search");
+        
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("message", "Le paramètre 'artist' est requis.");
+    });
+});
