db42142b11a114277b7273a446b2a3abf630a8e1
david <dahodprod@gmail.com>
2025-02-04 11:20:48 +0100

    📦 NEW:  add genreController

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,6 @@ import { firstController } from '../controllers/index.js';
 const router = express.Router();
 
 router.get('', firstController);
+router.get('/genre', genreController);
 
 export default router;
