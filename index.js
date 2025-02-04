aec1a9731bfb84b7436afdca457734109740b450
Anthony <anthony20021@gmail>
2025-02-04 10:17:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from 'dotenv';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, Express !");
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
