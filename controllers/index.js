d5cc25461644048d05fe729edb52a4673946b1e0
MehdiSDK <mehdi@sudokeys.com>
2025-02-04 11:56:09 +0100

    👌 IMPROVE: ajout de la route vers chart, nettoyage du code

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,3 @@
 export const firstController = async (req, res) => {
     res.send("Bienvenue dans l'API musique !");
 };
-
-export const getCharts = async (req, res) => {
-    const { name } = req.params;
-    fetch('https://api.deezer.com/chart')
-        .then(response => response.json()) // Convertir la réponse en JSON
-        .then(data => console.log(data)) // Afficher les données
-        .catch(error => console.error('Erreur :', error)); // Gérer les erreurs
-};
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { firstController } from '../controllers/index.js';
 import { searchArtist } from "../controllers/searchController.js";
+import { getCharts } from "../controllers/getController.js";
 
 const router = express.Router();
 
 router.get('', firstController);
 router.get("/api/search", searchArtist);
+router.get("/charts", getCharts);
 
 export default router;
